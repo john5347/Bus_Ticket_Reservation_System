10d40a187960ffdca86a77d803b69268ec6e0a3f
john5347 <yohanisori1@gmail.com>
2023-05-12 22:51:02 +0300

    Update

--- passanger info.cpp
+++ passanger info.cpp
@@ -1,56 +1,31 @@
 #include<iostream>
 #include<string>
 using namespace std;
-enum city_store {bahirdar, adiss_abeba, debre_birehan, shewarobit, hawassa};
-	struct passanger{
+struct passanger{
    		 string first_name;
    		 string last_name;
    		 int age;
    		 int phone_number;
-   		 city_store city_destination;
-		 city_store city_departure; };
-int passanger_info_form(passanger){
-	passanger Person1;
-	cout<<"Passanger Information form";
-  cout<<"\nEnter passanger first name:";
-  cin>>Person1.first_name;
-  cout<<"\nEnter passanger last name";
-  cin>>Person1.last_name;
-  cout<<"\nEnter passanger age";
-  cin>>Person1.age;
-  cout<<"\nEnter passanger phone number";
-  cin>>Person1.phone_number;
-  return(Person1);	
- // cout<<"\nEnter passanger departure city";
-//  cin>>Person1.city_departure;
- //cout<<"\nEnter passanger destination city";
- // cin>>Person1.city_destination;
-  
-}
-void passanger_(passanger){
-	passanger Person1;
-	cout<<"";
-  cout<<"\nEnter passanger first name:";
-  cin>>Person1.first_name;
-  cout<<"\nEnter passanger last name";
-  cin>>Person1.last_name;
-  cout<<"\nEnter passanger age";
-  cin>>Person1.age;
-  cout<<"\nEnter passanger phone number";
-  cin>>Person1.phone_number;
-}
+   		 int seat;
+   		 }s[30];//for 30 seat bus
 int main(){
 	int a;
-	passanger abebe;
 	cout<<"This is passanges information form. Do you want continue?"<<"\n                        yes=1    No =0\n";
 	cin>>a;
 	if(a){
-		passanger_info_form(abebe);
-	}
+		for (int i=0; i<10; i++){
+			s[i].seat=i+1;
+			cout<<"For seat Number"<<s[i].seat<<",\n";
+			cout<<"Enter First Name: ";
+			cin>>s[i].first_name;
+			cout<<"Enter Last Name: ";
+			cin>>s[i].last_name;
+			cout<<"Enter Age: ";
+			cin>>s[i].age;
+			cout<<"Enter Phone Number: ";
+			cin>>s[i].phone_number;
+			cout<<endl;
+			
+		}
+	}}
 	
- // cout<<"\nEnter passanger departure city";
-//  cin>>Person1.city_departure;
- //cout<<"\nEnter passanger destination city";
- // cin>>Person1.city_destination;
-  
-}
